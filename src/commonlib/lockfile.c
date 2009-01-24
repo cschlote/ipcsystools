b5a7c03eaf58b07aed452ece2953c1ccb27b37e7
cschlote <cschlote@89badfba-9fa3-485e-9e33-7f56615c647f>
2009-01-24 13:48:25 +0000

    - svn:ignore gesetzt - Kleine Erweiterungen des autoconf Skripte
    git-svn-id: http://kplanas01/svn/mcbsystools/trunk@3 89badfba-9fa3-485e-9e33-7f56615c647f

--- src/commonlib/lockfile.c
+++ src/commonlib/lockfile.c
@@ -30,6 +30,7 @@
 
 int CreateLockfile(const char* strLockfile, const char* strDevice)
 {
+	int rc;
 	int bRet = 0;
 
 	int nLockFD = open(strLockfile, O_RDWR | O_CREAT | O_EXCL, 0444);
@@ -38,7 +39,7 @@ int CreateLockfile(const char* strLockfile, const char* strDevice)
 		// Lockfile does not exist
 		char strPid [ 12 ];
 		sprintf(strPid, "%10d\n", getpid());
-		write(nLockFD, strPid, strlen(strPid));
+		rc = write(nLockFD, strPid, strlen(strPid));
 		close(nLockFD);
 		bRet = 1;
 		syslog(LOG_DEBUG, "Locking serial device '%s'", strDevice);
