25d86ff04c6c15d2ec2475e4775d6b7da44d7eea
cschlote <cschlote@89badfba-9fa3-485e-9e33-7f56615c647f>
2010-07-11 17:38:07 +0000

    [mcbsystools] Big Bugfixing Summer Session
    First set of bugfixes
    - gpsstatus error codes get extracted
    - CRLF problem with modem output solved with tr -d '\r'
    - Added option handling
    - Added start, stop, status, monitor functions
    
    Signed-off-by: Carsten Schlote <c.schlote@konzeptpark.de>
    
    git-svn-id: http://kplanas01/svn/mcbsystools/trunk@94 89badfba-9fa3-485e-9e33-7f56615c647f

--- src/utils/scripts/gps-monitor.sh
+++ src/utils/scripts/gps-monitor.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -u -x
+#!/bin/bash
+
 #
 # GPS Function Library
 #
@@ -8,7 +9,11 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
 DESC="gps-monitor[$$]"
 
-MCB_MONITOR_PID_FILE=/var/lock/mcb-monitor.pid
+GPS_STATUS_FILE=$MCB_STATUSFILE_DIR/gps_status
+GPS_LOCATION_FILE=$MCB_STATUSFILE_DIR/gps_location
+GPS_SATINFO_FILE=$MCB_STATUSFILE_DIR/gps_satinfo
+
+MCB_MONITOR_PID_FILE=/var/lock/gps-monitor.pid
 
 # ----------------------------------------------------------------------
 # Errorcodes
@@ -97,40 +102,59 @@ GPS_ErrorCodeFix=(
 #             PCSCDISABLE         0x03
 #             GPSENABLE           0x01  (or not displayed when not configured)
 # OK
-$MCB_TOOLS_DIR/umtscardtool -s ATZ
+function gps_checkmodem_feature ()
+{
+    syslogger "debug" "Check modem caps"
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!custom? | grep -q GPSENABLE; then
+	syslogger "error" "Modem has no GPS feature support"
+	return 1
+    fi
+    return 0
+}
 
-# at!entercnd="A710"
-# at!custom="GPSENABLE",1
-# at!greset
-
-# ----------------------------------------------------------------------
-# -- ContinusStart Tracking to ttyUSB2
-# ----------------------------------------------------------------------
-
-# !GPSTRACK: <fixtype>,<maxtime>,<maxdist>,<fixcount>,<fixrate>
+# at!gpsautostart=?
+# !GPSAUTOSTART: <enable>[,<fixtype>,<maxtime>,<maxdist>,<fixrate>]
+# <enable>:  0-Disabled, 1-Enabled
 # <fixtype>: 1-Standalone, 2-MS-Based, 3-MS-Assisted
-# <maxtime>: 0-255 seconds
-# <maxdist>: 0-4294967280 meters
-# <fixcount>: 1-1000,1000=continuous
-# <fixrate>: 1-1,799,999 seconds
+# <maxtime>: 1-255 seconds
+# <maxdist>: 1-4294967280 meters
+# <fixrate>: 1-65535 seconds
+function gps_gpsautostart ()
+{
+    local ena=1; [ -n "$1" ] && mode=$1
+    local mode=1; [ -n "$2" ] && mode=$2
+    local to=15; [ -n "$3" ] && to=$3
+    local dist=30; [ -n "$4" ] && dist=$4
+    local fr=10; [ -n "$4" ] && dist=$4
+    syslogger "debug" "Send at!gpsautostartx=$ena,$mode,$to,$dist,$fr"
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpsautostartx=$ena,$mode,$to,$dist,$fr | grep -q OK; then
+	syslogger "error" "Modem failed on !gpsautostartx command"
+	return 1
+    fi
+    return 0
+}
 
-# AT!GPSTRACK=1,255,30,1000,1
-
-# ----------------------------------------------------------------------
-# -- Standalone fix
-# ----------------------------------------------------------------------
 
 # AT!GPSFIX=?
 # AT!GPSFIX: <fixtype>,<maxtime>,<maxdist>
 # <fixtype>: 1-Standalone, 2-MS-Based, 3-MS-Assisted
 # <maxtime>: 0-255 seconds
 # <maxdist>: 0-4294967280 meters
-
 # AT!GPSFIX=1,15,10
-
-# ----------------------------------------------------------------------
-# -- Monitor current fix status
-# ----------------------------------------------------------------------
+# 
+# OK
+function gps_gpsfix ()
+{
+    local mode=1; [ -n "$1" ] && mode=$1
+    local to=15; [ -n "$2" ] && to=$2
+    local dist=30; [ -n "$3" ] && dist=$3
+    syslogger "debug" "Send at!gpsfix=$mode,$to,$dist command "
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpsfix=$mode,$to,$dist | grep -q OK; then
+	syslogger "error" "Modem failed on !gpsfix command"
+	return 1
+    fi
+    return 0
+}
 
 # AT!GPSSTATUS?
 # Current time: <y> <m> <d> <dow> <time>
@@ -142,10 +166,15 @@ $MCB_TOOLS_DIR/umtscardtool -s ATZ
 #
 # OK
 # <status>:=NONE ACTIVE SUCCESS FAIL
-
-# ----------------------------------------------------------------------
-# -- Query current position
-# ----------------------------------------------------------------------
+function gps_gpsstatus ()
+{
+    syslogger "debug" "Send at!gpsstatus? command"
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpsstatus?; then
+	syslogger "error" "!gpsstatus reports failed"
+	return 1
+    fi
+    return 0
+}
 
 # AT!GPSLOC?
 # Lat: 50 Deg 34 Min 58.69 Sec N  (0x008FE165)
@@ -156,12 +185,235 @@ $MCB_TOOLS_DIR/umtscardtool -s ATZ
 # Altitude: 217  LocUncVe: 20
 #
 # OK
+function gps_gpsloc ()
+{
+    syslogger "debug" "Send at!gpsloc? command "
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpsloc?; then
+	syslogger "error" "!gpsloc reports failed"
+	return 1
+    fi
+    return 0
+}
+
+# !GPSTRACK: <fixtype>,<maxtime>,<maxdist>,<fixcount>,<fixrate>
+# <fixtype>: 1-Standalone, 2-MS-Based, 3-MS-Assisted
+# <maxtime>: 0-255 seconds
+# <maxdist>: 0-4294967280 meters
+# <fixcount>: 1-1000,1000=continuous
+# <fixrate>: 1-1,799,999 seconds
+
+# AT!GPSTRACK=1,255,30,1000,1
+function gps_gpstrack ()
+{
+    local mode=1; [ -n "$1" ] && mode=$1
+    local to=255; [ -n "$2" ] && to=$2
+    local dist=30; [ -n "$3" ] && dist=$3
+    local fc=1000; [ -n "$4" ] && fc=$4
+    local fr=1; [ -n "$5" ] && fr=$5
+    syslogger "debug" "Send at!gpstrack=$mode,$to,$dist,$fc,$fr command "
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpstrack=$mode,$to,$dist,$fc,$fr | tr -d '\r'; then
+	syslogger "error" "!gpstrack reports failed"
+	return 1
+    fi
+    return 0
+}
+
+function gps_gpssatinfo ()
+{
+    syslogger "debug" "Send at!gpssatinfo? command "
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpssatinfo? | tr -d '\r'; then
+	syslogger "error" "!gpssatinfo? failed"
+	return 1
+    fi
+    return 0
+}
+
+function gps_gpsend ()
+{
+    syslogger "debug" "Send at!gpsend=0 command "
+    if ! $MCB_TOOLS_DIR/umtscardtool -s at!gpsend=0 | tr -d '\r'; then
+	syslogger "error" "!gpsend=0 failed"
+	return 1
+    fi
+    return 0
+}
+
+#-----------------------------------------------------------------------
+# GPS functions
+#-----------------------------------------------------------------------
+
+GPS_TIME=
+GPS_LASTSTATUS=
+GPS_CURRSTATUS=
+GPS_LASTERROR=
+GPS_CURRERROR=
+GPS_TTFF=
+
+function save_gpsstatus ()
+{
+    gps_gpsstatus | tr -d '\r' | grep -v "OK" >$GPS_STATUS_FILE
+}
+function save_gpslocation ()
+{
+    gps_gpsloc | tr -d '\r' | grep -v "OK" >$GPS_LOCATION_FILE
+}
+function save_gpssatinfo ()
+{
+    gps_gpssatinfo | tr -d '\r' | grep -v "OK" >$GPS_SATINFO_FILE
+}
+function get_errorstring ()
+{
+    if [ -n "$1" ]; then
+	echo ${GPS_Errorcode[$1]}
+    fi
+}
+function query_gpsstatus ()
+{
+    save_gpsstatus
+    cat $GPS_STATUS_FILE
+    local t1=`cat $GPS_STATUS_FILE | sed -n "s/^.*Last Fix Status    = \(.*\)$/\1/ p"`
+    local t2=`cat $GPS_STATUS_FILE | sed -n "s/^.*Fix Session Status = \(.*\)$/\1/ p"`
+    GPS_LASTSTATUS=$t1
+    GPS_CURRSTATUS=$t2
+    GPS_LASTERROR=`echo $t1 | sed -n "s/.*FAILCODE = \(.*\)$/\1/ p"`
+    GPS_CURRERROR=`echo $t2 | sed -n "s/.*FAILCODE = \(.*\)$/\1/ p"`
+    echo "Last status: $GPS_LASTSTATUS `get_errorstring $GPS_LASTERROR`"
+    echo "Curr status: $GPS_CURRSTATUS `get_errorstring $GPS_CURRERROR`"
+}
+function query_gpsloc ()
+{
+    save_gpslocation
+    cat $GPS_LOCATION_FILE
+}
+function query_gpssatinfo ()
+{
+    save_gpssatinfo
+    cat $GPS_SATINFO_FILE
+}
 
 
+function issue_gpsfix ()
+{
+    gps_gpsfix "$1" "$2" "$3"
+}
+function issue_gpstrack ()
+{
+    gps_gpstrack "$1" "$2" "$3" "$4" 
+}
+function wait_gpsfix ()
+{
+    local rc=0
+    while true; do
+	query_gpsstatus
+	case "$GPS_CURRSTATUS" in
+	NONE)
+	    syslogger "error" "No gpsfix started"
+	    rc=1; break ;;
+	ACTIVE)
+	    syslogger "debug" "gpsfix started and active"
+	    ;;
+	SUCCESS)
+	    syslogger "debug" "gpsfix succesfully finished"
+	    rc=0; break;;
+	FAIL*)
+	    syslogger "debug" "gpsfix failed"
+	    rc=1; break;;
+	*)
+	    syslogger "debug" "unknown currstatus $GPS_CURRSTATUS"
+	    rc=1; break
+	esac
+
+#	case "$GPS_LASTSTATUS" in
+#	NONE*)		;;
+#	SUCCESS*)	;;
+#	FAIL*)		;;
+#	*)
+#	    syslogger "debug" "unknown laststatus"
+#	    rc=1; break
+#	esac
+
+	sleep 10
+    done
+
+    if [ $rc = 1 ]; then
+	syslogger "error" "!gpsstatus reports failed or none fixup"
+	#local ec=`echo ${status/\r/} | awk '{print $13}'`
+	#echo "Error $ec := ${GPS_Errorcode[$ec]}"
+	return 1
+    fi
+    return 0
+}
 
 #-----------------------------------------------------------------------
 # MCB GPS Monitor
 #-----------------------------------------------------------------------
+
+function gps_start ()
+{
+    # --- Get an intitial GPS fix ----------------------------
+    query_gpsstatus
+    if [ "$GPS_CURRSTATUS" = "ACTIVE" ]; then
+	syslogger "debug" "Stopping active GPS Fix session"
+	gps_gpsend
+    fi
+
+    local retries=3
+    while [ $retries -gt 0 ]; do
+	syslogger "debug" "Trying to obtain initial fix (retries=$retries)"
+	issue_gpsfix 1 255 30
+	wait_gpsfix
+	retries=$[$retries-1]
+	query_gpsloc
+	query_gpssatinfo
+    done
+
+    if [ $retries -eq 0 ]; then
+	syslogger "error" "Unable to get GPS fix, yet"
+    fi
+
+    # --- Start NMEA output on USB port
+    issue_gpstrack
+
+    if ! pidof gpsd; then
+	gpsd -D2 -P /var/run/gpsd.pid /dev/ttyUSB2 &
+    fi
+}
+
+function gps_stop ()
+{
+    if pidof gpsd && [ -e /var/run/gpsd.pid ]; then
+	kill -TERM 'cat /var/run/gpsd.pid'
+	rm /var/run/gpsd.pid
+    fi
+}
+
+function gps_monitor ()
+{
+    query_gpsstatus
+    query_gpsloc
+    query_gpssatinfo
+}
+
+function print_usage {
+    echo "Usage: -h <start|stop|monitor>"
+}
+
+echo -e "\nkonzeptpark GPS monitor\n"
+
+while getopts "hfd:s:r:" optionName; do
+	case "$optionName" in
+	h) print_usage; exit 1;;
+#	f) force=1;;			# FIXME: Set values for to,dist, ...
+#	d) device="$OPTARG";;
+#	s) serverip="$OPTARG";;
+#	r) versionpath="$OPTARG";;
+	[?]) print_usage; exit 1;;
+	esac
+done
+
+if ! [ $# -ge 1 ]; then
+    print_usage; exit 1
+fi
 
 obtainlock $MCB_MONITOR_PID_FILE
 syslogger "debug" "Started monitor (`date`)"
@@ -169,11 +421,17 @@ syslogger "debug" "Started monitor (`date`)"
 # Update files and links for GSM modem connection
 WriteGSMConnectionInfoFiles
 
-#-- Start of script ----------------------------------------------------
+if gps_checkmodem_feature; then
 
+    case "$1" in
+	"start")	gps_start ;;
+	"stop")	gps_stop ;;
+	"monitor")	gps_monitor ;;
+    esac
 
+else
+    echo "Modem doesn't support GPS"
+fi
 
-#-- End of script ------------------------------------------------------
 syslogger "debug" "Finished monitor (`date`)"
 releaselock
-exit 0
