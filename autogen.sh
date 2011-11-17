cf48b9cdb9fb6d5721f5663f71ce849d8c851350
Carsten Schlote <c.schlote@konzeptpark.de>
2011-11-17 15:37:30 +0100

    [ubuntu-port] Updated to local 10.04.3 LTS autotools, make distclean
    - Run autoreconf on sources
    - make distclean
    
    Signed-off-by: Carsten Schlote <c.schlote@konzeptpark.de>

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,9 @@
 
 aclocal $ACLOCAL_FLAGS
 
-libtoolize --force --copy
-autoheader
-automake
 autoreconf \
 	--force \
 	--install \
-	--warnings=cross \
 	--warnings=syntax \
 	--warnings=obsolete \
 	--warnings=unsupported
