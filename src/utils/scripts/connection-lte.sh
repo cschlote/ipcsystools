96fb8b6b288f0ea0273c51ca1bb975cf1aa9ec74
Carsten Schlote <c.schlote@konzeptpark.de>
2012-05-21 15:53:31 +0200

    [ipcsystools] Crossplatform fixes
    - Changed code to work with MCB2 ptxdist bash and busybox
    - Readded code to install a busybox crontab instead of /etc/cron.d
      on MCB2 only.
    
    Signed-off-by: Carsten Schlote <c.schlote@konzeptpark.de>

--- src/utils/scripts/connection-lte.sh
+++ src/utils/scripts/connection-lte.sh
@@ -133,9 +133,9 @@ case "$cmd" in
 start)
 	syslogger "info" "starting connection..."
 	ReadModemStatusFile
-	if 	[ $MODEM_STATUS == ${MODEM_STATES[detectedID]} ] ||
-		[ $MODEM_STATUS == ${MODEM_STATES[readyID]} ] ||
-		[ $MODEM_STATUS == ${MODEM_STATES[registeredID]} ]; then
+	if 	[ "$MODEM_STATUS" = "${MODEM_STATES[detectedID]}" ] ||
+		[ "$MODEM_STATUS" = "${MODEM_STATES[readyID]}" ] ||
+		[ "$MODEM_STATUS" = "${MODEM_STATES[registeredID]}" ]; then
 
 	    # LED 3g Timer blinken
 	    $IPC_SCRIPTS_DIR/set_fp_leds 3g timer
